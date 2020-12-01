e8ad0435fd1364b1d27f527874668c8f60dd815d
Mathijs de Langen <mathijs.dla@gmail.com>
2020-12-01 08:53:41 +0100

    Use 4 spaces for indent, as is dictated by the rust coding guidelines

--- 2020/dec_01/src/main.rs
+++ 2020/dec_01/src/main.rs
@@ -1,24 +1,24 @@
 mod constants;
 
 fn find_sum(number_of_values: u32, sum: i32) -> i32 {
-  let mut result = 0;
-  for value in constants::INPUT.iter(){
-    let find_value = sum - value;
-    if number_of_values > 1 {
-      let sum2 = find_sum(number_of_values-1, find_value);
-      if sum2 > 0 {
-        result = value*sum2;
-      }
-      else {
-        if constants::INPUT.contains(&find_value){
-          result = value * find_value;
-        }
-      }
-    }
-  }
-  return result;
+	let mut result = 0;
+	for value in constants::INPUT.iter(){
+		let find_value = sum - value;
+		if number_of_values > 1 {
+			let sum2 = find_sum(number_of_values-1, find_value);
+			if sum2 > 0 {
+				result = value*sum2;
+			}
+			else {
+				if constants::INPUT.contains(&find_value){
+					result = value * find_value;
+				}
+			}
+		}
+	}
+	return result;
 }
 
 fn main() {
-  print!("{}",find_sum(2, 2020));
+	print!("{}",find_sum(2, 2020));
 }
