e6b6b5f17f3928caf57830737ecea371010705f9
Mathijs de Langen <mathijs.dla@gmail.com>
2020-12-13 12:44:41 +0100

    Solution 2

--- 2020/dec_13/src/main.rs
+++ 2020/dec_13/src/main.rs
@@ -1,34 +1,56 @@
 use std::fs;
 
-fn parse_input(input_file: &str) -> (u32, Vec<u32>) {
+// Return vec from bus_id -> added timestamp
+fn parse_input(input_file: &str) -> Vec<(u128,u128)> {
     let input = fs::read_to_string(input_file).expect("Something went wrong reading the file");
     let input_lines : Vec<&str> = input.split("\n").map(|v| v.trim()).collect();
 
-    let earliest_timestamp = input_lines[0].parse::<u32>().unwrap();
-    let busses : Vec<u32> = input_lines[1].split(",")
-                                        .filter(|&v| v != "x")
-                                        .map(|v| v.parse::<u32>().unwrap())
-                                        .collect();
-    return (earliest_timestamp,busses)
-
-}
-
-fn determine_earliest_depart(timestamp: u32, busses: Vec<u32>) -> (u32, u32) {
-    let mut result = 1024;
-    for bus in busses {
-        if bus - (timestamp % bus) < result - (timestamp % result) {
-            result = bus;
+    let busses : Vec<&str> = input_lines[1].split(",").collect();
+    let mut busses_with_timestamps = Vec::new();
+    for t in 0..busses.len() {
+        match busses[t] {
+            "x" => (),
+            _   => busses_with_timestamps.push((busses[t].parse::<u128>().unwrap(), t as u128)),
         }
     }
+    return busses_with_timestamps;
+}
 
-    return (result, result - (timestamp % result) + timestamp);
+fn check_timestamp(current_time: u128, bus_with_timestamp: &(u128,u128)) -> bool {
+    let bus = bus_with_timestamp.0;
+    let timestamp = bus_with_timestamp.1;
+
+    return (current_time + timestamp) % bus == 0;
+}
+
+fn update_incrementer(current_increment: u128, bus_id: u128) -> u128 {
+    return current_increment * bus_id;
+}
+
+fn determine_earliest_timestap(busses: &Vec<(u128,u128)>) -> u128 {
+    let mut current_timestamp = 0;
+    let mut incrementer = 1;
+    for bus in busses {
+        // Find combined schedule with new bus
+        loop {
+            if check_timestamp(current_timestamp, bus) { 
+                println!("Timestamp OK @ {} for {} (with t+{})", current_timestamp, bus.0, bus.1);
+                incrementer = update_incrementer(incrementer, bus.0); 
+                println!("Incrementer set to {}", incrementer);
+                break; 
+            }
+            else {
+                current_timestamp += incrementer
+            }
+        } 
+    }
+
+    return current_timestamp;
 }
 
 fn main() {
     let input_file = "assets/dec_13.in";
-    let (earliest_timestamp,busses) = parse_input(input_file);
-    let (bus, departure_time) = determine_earliest_depart(earliest_timestamp, busses);
-    println!("Solution 1: {}", (departure_time - earliest_timestamp) * bus);
+    println!("Solution: {}", determine_earliest_timestap(&parse_input(input_file)));
 }
 
 #[cfg(test)]
@@ -36,11 +58,12 @@ mod tests {
     use super::*;
     
     #[test]
-    fn validate_example1() {
-        let input_file = "assets/dec_13_example.in";
-        let (earliest_timestamp,busses) = parse_input(input_file);
-        let (bus, departure_time) = determine_earliest_depart(earliest_timestamp, busses);
-
-        assert_eq!(295, (departure_time - earliest_timestamp) * bus);
+    fn validate_example() {
+        assert_eq!(1068781, determine_earliest_timestap(&parse_input("assets/dec_13_example.in")));
+        assert_eq!(3417, determine_earliest_timestap(&parse_input("assets/dec_13_example1.in")));
+        assert_eq!(754018, determine_earliest_timestap(&parse_input("assets/dec_13_example2.in")));
+        assert_eq!(779210, determine_earliest_timestap(&parse_input("assets/dec_13_example3.in")));
+        assert_eq!(1261476, determine_earliest_timestap(&parse_input("assets/dec_13_example4.in")));
+        assert_eq!(1202161486, determine_earliest_timestap(&parse_input("assets/dec_13_example5.in")));
     }
 }
