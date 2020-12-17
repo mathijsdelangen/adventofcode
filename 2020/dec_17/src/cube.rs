28876d5b849e7317ce51e2070999701ab62132c4
Mathijs de Langen <mathijs.dla@gmail.com>
2020-12-17 15:15:41 +0100

    Change Point3 -> Point4 for solution 2

--- 2020/dec_17/src/cube.rs
+++ 2020/dec_17/src/cube.rs
@@ -1,42 +1,45 @@
-pub struct Point3 {
+pub struct Point4 {
     pub x: usize,
     pub y: usize,
     pub z: usize,
+    pub w: usize
 }
 
-impl Point3 {
-    pub fn is_neighbor(self, other: &Point3) -> bool {
+impl Point4 {
+    pub fn is_neighbor(self, other: &Point4) -> bool {
         return (self.x as i32 - other.x as i32 ).abs() <= 1 
             && (self.y as i32 - other.y as i32 ).abs() <= 1 
             && (self.z as i32 - other.z as i32 ).abs() <= 1
+            && (self.w as i32 - other.w as i32 ).abs() <= 1
             && *other != self;
     }
 }
 
-impl PartialEq for Point3 {
+impl PartialEq for Point4 {
     fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y && self.z == other.z
+        self.x == other.x && self.y == other.y && self.z == other.z && self.w == other.w
     }
 }
 
-impl Copy for Point3 { }
-impl Clone for Point3 {
-    fn clone(&self) -> Point3 {
-        Point3 {x: self.x, y: self.y, z: self.z}
+impl Copy for Point4 { }
+impl Clone for Point4 {
+    fn clone(&self) -> Point4 {
+        Point4 {x: self.x, y: self.y, z: self.z, w: self.w}
     }
 }
 
 pub struct Cube {
     pub width: usize,
     pub height: usize,
-    pub occupancies : Vec<Point3>,
+    pub max_w: usize,
+    pub occupancies : Vec<Point4>,
 }
 
-pub fn is_present(occupancies: &Vec<Point3>, p: &Point3) -> bool {
-    return occupancies.iter().any(|o| o.x == p.x && o.y == p.y && o.z == p.z);
+pub fn is_present(occupancies: &Vec<Point4>, p: &Point4) -> bool {
+    return occupancies.iter().any(|o| o.x == p.x && o.y == p.y && o.z == p.z && o.w == p.w);
 }
 
-pub fn count_occupied_neighbors(occupancies: &Vec<Point3>, p : Point3) -> usize {
+pub fn count_occupied_neighbors(occupancies: &Vec<Point4>, p : Point4) -> usize {
     return occupancies.iter().filter(|&o| p.is_neighbor(o)).count();
 }
 
@@ -49,23 +52,25 @@ impl Cube {
         for (y, line) in lines.iter().enumerate() {
             for (x,c) in line.chars().enumerate() {
                 match c {
-                    '#' => points_in_cube.push(Point3 {x: (x+1), y: (y+1), z: 1}),
+                    '#' => points_in_cube.push(Point4 {x: (x+1), y: (y+1), z: 1, w: 1}),
                     _  => (),
                 }
             }
         }
 
-        return Cube {width: width, height: 3, occupancies: points_in_cube}
+        return Cube {width: width, height: 3, max_w: 3, occupancies: points_in_cube}
     }
 
     pub fn print(&self) {
-        for z in 0..self.height {
-            println!("At height z={}", (z as i32 - self.height as i32 / 2));
-            for y in 0..self.width {
-                for x in 0..self.width {
-                    if is_present(&self.occupancies,&Point3 {x:x, y:y, z:z}) { print!("#") }else { print!(".") }
+        for w in 0..self.max_w {
+            for z in 0..self.height {
+                println!("At height z={}, w={}", (z as i32 - self.height as i32 / 2), (w as i32 - self.max_w as i32 / 2));
+                for y in 0..self.width {
+                    for x in 0..self.width {
+                        if is_present(&self.occupancies,&Point4 {x:x, y:y, z:z, w:w}) { print!("#") }else { print!(".") }
+                    }
+                    println!();
                 }
-                println!();
             }
         }
     }
@@ -77,8 +82,8 @@ mod tests {
 
     #[test]
     fn validate_is_neighbor(){
-        assert_eq!(true, Point3 {x:1, y:2, z:3}.is_neighbor(&Point3 {x:2, y:2, z:2}));
-        assert_eq!(true, Point3 {x:1, y:2, z:3}.is_neighbor(&Point3 {x:0, y:2, z:3}));
-        assert_eq!(false, Point3 {x:2, y:2, z:2}.is_neighbor(&Point3 {x:2, y:2, z:2}));
+        assert_eq!(true, Point4 {x:1, y:2, z:3, w:0}.is_neighbor(&Point4 {x:2, y:2, z:2, w:0}));
+        assert_eq!(true, Point4 {x:1, y:2, z:3, w:0}.is_neighbor(&Point4 {x:0, y:2, z:3, w:0}));
+        assert_eq!(false, Point4 {x:2, y:2, z:2, w:0}.is_neighbor(&Point4 {x:2, y:2, z:2, w:0}));
     }
 }
--- 2020/dec_17/src/main.rs
+++ 2020/dec_17/src/main.rs
@@ -3,21 +3,23 @@ use std::fs;
 mod cube;
 
 fn simulate_cycle(orig: &cube::Cube) -> cube::Cube {
-    let mut cube = cube::Cube {width: orig.width+2, height: orig.height+2, occupancies: vec![]};
+    let mut cube = cube::Cube {width: orig.width+2, height: orig.height+2, max_w: orig.max_w+2, occupancies: vec![]};
     let mut points_in_cube = Vec::new();
 
-    for z in 0..cube.height {
-        for y in 0..cube.width {
-            for x in 0..cube.width {
-                let p = cube::Point3 {x:x, y:y, z:z};
-                let active_neighors = cube::count_occupied_neighbors(&orig.occupancies, p);
-                if cube::is_present(&orig.occupancies, &p) {
-                    if active_neighors == 2 || active_neighors == 3 {
-                        points_in_cube.push(cube::Point3 {x: p.x+1, y: p.y+1, z: p.z+1});
-                    }
-                } else {
-                    if active_neighors == 3 {
-                        points_in_cube.push(cube::Point3 {x: p.x+1, y: p.y+1, z: p.z+1});
+    for w in 0..cube.max_w {
+        for z in 0..cube.height {
+            for y in 0..cube.width {
+                for x in 0..cube.width {
+                    let p = cube::Point4 {x:x, y:y, z:z, w:w};
+                    let active_neighors = cube::count_occupied_neighbors(&orig.occupancies, p);
+                    if cube::is_present(&orig.occupancies, &p) {
+                        if active_neighors == 2 || active_neighors == 3 {
+                            points_in_cube.push(cube::Point4 {x: p.x+1, y: p.y+1, z: p.z+1, w:p.w+1});
+                        }
+                    } else {
+                        if active_neighors == 3 {
+                            points_in_cube.push(cube::Point4 {x: p.x+1, y: p.y+1, z: p.z+1, w:p.w+1});
+                        }
                     }
                 }
             }
@@ -58,6 +60,6 @@ mod tests {
         let input_file = "assets/dec_17_example.in";
         let input = fs::read_to_string(input_file).expect("Something went wrong reading the file");
         let cube = cube::Cube::new_from_str(&input);
-        assert_eq!(112, determine_active_cubes(cube));
+        assert_eq!(848, determine_active_cubes(cube));
     }
 }
