182a552189cb81ae43801ae26f90fe7e5db8c383
Mathijs de Langen <mathijs.dla@gmail.com>
2020-12-22 18:43:40 +0100

    Puzzle solved

--- 2020/dec_20/src/main.rs
+++ 2020/dec_20/src/main.rs
@@ -44,10 +44,16 @@ fn find_neighbors(tile: &puzzle::Tile, tiles: &Vec<puzzle::Tile>) -> Vec<u32> {
     return neighbors;
 }
 
-fn get_tile_by_id(id: u32, tiles: &Vec<puzzle::Tile>) -> &puzzle::Tile {
+fn get_tile_by_id(id: u32, tiles: Vec<puzzle::Tile>) -> puzzle::Tile {
     for tile in tiles { if tile.id == id { return tile}}
     panic!("No tile found")
 }
+
+fn get_tile(id: u32, tiles: Vec<&puzzle::Tile>) -> &puzzle::Tile {
+    for tile in tiles { if tile.id == id { return tile}}
+    panic!("No tile found")
+}
+
 
 fn print_puzzle(puzzle_solution: &[[u32;12];12]) {
     for a in puzzle_solution {
@@ -58,18 +64,33 @@ fn print_puzzle(puzzle_solution: &[[u32;12];12]) {
     }
 }
 
-fn print_puzzle_pieces(puzzle_solution: &[[u32;12];12], tiles: &Vec<puzzle::Tile>) {
-    for a in puzzle_solution {
+fn print_puzzle_pieces(puzzle_positions: &[[u32;12];12], tiles: &Vec<puzzle::Tile>) {
+    for a in puzzle_positions {
         for line in 0..10 {
             for b in a { 
                 if *b == 0 {continue}
-                let tile = get_tile_by_id(*b, &tiles);
+                let tile = get_tile_by_id(*b, tiles.to_vec());
                 
                 print!("{}|", tile.tile_description[line]);
             }
             println!();
         }
         println!("----------");
+    }
+}
+
+fn print_puzzle_tiles(puzzle_tiles : &Vec<Vec<puzzle::Tile>>){
+    for line in puzzle_tiles {
+        if line.len() > 0
+        {
+            for idx in 0..line[0].tile_description.len() {
+                for tile in line {
+                    print!("{}|", tile.tile_description[idx]);
+                }
+                println!();
+            }
+            println!("----------");
+        }
     }
 }
 
@@ -94,10 +115,10 @@ fn solve_puzzle_positions(tiles: &Vec<puzzle::Tile>) -> [[u32;12];12] {
             println!("{} ids left", puzzle_ids_left.len());
             println!("Find tile id at position ({},{}) for ID {}", i, iteration-1, puzzle_solution[i][iteration-1]);
 
-            let neighbors_1 = find_neighbors(get_tile_by_id(puzzle_solution[i][iteration-1], &tiles),&tiles);
+            let neighbors_1 = find_neighbors(&get_tile_by_id(puzzle_solution[i][iteration-1], tiles.to_vec()),&tiles);
             let mut allowed_ids = Vec::new();
             if i > 0 {
-                let neighbors_2 = find_neighbors(get_tile_by_id(puzzle_solution[i-1][iteration], &tiles),&tiles);
+                let neighbors_2 = find_neighbors(&get_tile_by_id(puzzle_solution[i-1][iteration], tiles.to_vec()),&tiles);
                 for neighbor in neighbors_2 {
                     if neighbors_1.contains(&neighbor) {
                         allowed_ids.push(neighbor);
@@ -121,10 +142,10 @@ fn solve_puzzle_positions(tiles: &Vec<puzzle::Tile>) -> [[u32;12];12] {
 
         for i in 0..iteration {
             print_puzzle(&puzzle_solution);
-            let neighbors_1 = find_neighbors(get_tile_by_id(puzzle_solution[iteration-1][i], &tiles),&tiles);
+            let neighbors_1 = find_neighbors(&get_tile_by_id(puzzle_solution[iteration-1][i], tiles.to_vec()),&tiles);
             let mut allowed_ids = Vec::new();
             if i > 0 {
-                let neighbors_2 = find_neighbors(get_tile_by_id(puzzle_solution[iteration][i-1], &tiles),&tiles);
+                let neighbors_2 = find_neighbors(&get_tile_by_id(puzzle_solution[iteration][i-1], tiles.to_vec()),&tiles);
                 for neighbor in neighbors_2 {
                     if neighbors_1.contains(&neighbor) {
                         allowed_ids.push(neighbor);
@@ -146,8 +167,8 @@ fn solve_puzzle_positions(tiles: &Vec<puzzle::Tile>) -> [[u32;12];12] {
         }
 
         {
-            let neighbors_1 = find_neighbors(get_tile_by_id(puzzle_solution[iteration-1][iteration], &tiles),&tiles);
-            let neighbors_2 = find_neighbors(get_tile_by_id(puzzle_solution[iteration][iteration-1], &tiles),&tiles);
+            let neighbors_1 = find_neighbors(&get_tile_by_id(puzzle_solution[iteration-1][iteration], tiles.to_vec()),&tiles);
+            let neighbors_2 = find_neighbors(&get_tile_by_id(puzzle_solution[iteration][iteration-1], tiles.to_vec()),&tiles);
             let mut puzzle_piece_id = 0;
             for neighbor in neighbors_1 {
                 if puzzle_ids_left.contains(&neighbor) && neighbors_2.contains(&neighbor) {puzzle_piece_id = neighbor}
@@ -163,41 +184,116 @@ fn solve_puzzle_positions(tiles: &Vec<puzzle::Tile>) -> [[u32;12];12] {
     return puzzle_solution;
 }
 
-fn determine_puzzle_output(puzzle_positions: &[[u32;12];12], tiles: &Vec<puzzle::Tile>) -> Vec<String> {
+fn nr_rotations_needed(request_border: usize, current_border: usize) -> isize {
+    return ((current_border as isize - request_border as isize) + 4) % 4;
+}
 
-    for i in 0..puzzle_positions.len()-1 {
-        for j in 0..puzzle_positions[i].len()-1 { 
-            if puzzle_positions[i][j+1] == 0 {continue}
-            
-            let pos_1 = puzzle_positions[i][j];
-            let pos_2 = puzzle_positions[i][j+1];
-            
-            let tile_1 = get_tile_by_id(pos_1, &tiles);
-            let tile_2 = get_tile_by_id(pos_2, &tiles);
+fn solve_puzzle_tiles(puzzle_positions: &[[u32;12];12], tile_list: Vec<puzzle::Tile>) -> Vec<Vec<puzzle::Tile>> {
+    let tiles = tile_list.to_owned();
 
-            let (nr_rot_1, nr_rot_2) = puzzle::Tile::find_common_border(tile_1,tile_2);
+    let mut solved_tiles : Vec<Vec<puzzle::Tile>> = Vec::new();
 
+    for j in 0..puzzle_positions.len() {
+        let mut solved_tiles_row = Vec::new();
+        for i in 0..puzzle_positions[j].len() { 
+            if puzzle_positions[i][j] == 0 {continue}
+
+            let pos_1 = puzzle_positions[i][j];            
+            // Special case, most left side -> Check pieze above
+            if i == 0 {
+                // Extra special case, starting piece
+                if j == 0 {
+                    let pos_2 = puzzle_positions[i+1][j];
+
+                    let mut tile = get_tile_by_id(pos_1, tile_list.to_vec());
+                    let neighbor = get_tile_by_id(pos_2, tile_list.to_vec());
+                    let neighbor_below = get_tile_by_id(puzzle_positions[i][j+1], tile_list.to_vec());
+
+                    // Find common border with piece right of this one
+                    let common_border = puzzle::Tile::find_common_border(&tile, &neighbor);
+                    println!("Common border between pos1 and right is ({},{})", common_border.0, common_border.1);
+                    println!("Tile should rotate {} (X -> 0) times", nr_rotations_needed(common_border.0, 0));
+                    for _ in 0..nr_rotations_needed(common_border.0, 0) { println!("Rotate");tile.rotate() }
+                    //println!("Neighbor should rotate {} (X -> 2) times", nr_rotations_needed(common_border.1, 2));
+                    //for _ in 0..nr_rotations_needed(common_border.1, 2) { neighbor.rotate() }
+
+                    // Find common border with piece below this one
+                    let common_border = puzzle::Tile::find_common_border(&tile, &neighbor_below);
+                    println!("Common border between pos1 and below is ({},{})", common_border.0, common_border.1);
+                    if common_border.0 != 1 { println!("Flip");tile.flip() }
+                    println!("Add tile");
+                    solved_tiles_row.push(tile);
+                }
+                else
+                {
+                    // Check pieze above                   
+                    let mut tile = get_tile_by_id(pos_1, tile_list.to_vec());
+                    let neighbor_above = solved_tiles.last().unwrap()[0].to_owned();
+
+                    let common_border = puzzle::Tile::find_common_border(&neighbor_above, &tile);
+                    println!("Common border between neighbor above and tile is ({},{})", common_border.0, common_border.1);
+                    println!("Tile should rotate {} (X -> 3) times", nr_rotations_needed(common_border.1, 3));
+                    for _ in 0..nr_rotations_needed(common_border.1, 3) { println!("Rotate");tile.rotate() }
+                    if tile.get_borders()[3] != neighbor_above.get_borders()[1] { println!("FlipAndRotate");tile.flip_and_rotate_back() }
+                    println!("Add tile");
+                    solved_tiles_row.push(tile);
+                }
+
+            }
+            else
+            {
+                // Check piece to the left               
+                let mut tile = get_tile_by_id(pos_1, tile_list.to_vec());
+                let left_neighbor = solved_tiles_row.last().unwrap().to_owned();
+
+                let common_border = puzzle::Tile::find_common_border(&left_neighbor, &tile);
+                println!("Common border between left_neighbor and tile is ({},{})", common_border.0, common_border.1);
+                println!("Tile should rotate {} (X -> 2) times", nr_rotations_needed(common_border.1, 2));
+                for _ in 0..nr_rotations_needed(common_border.1, 2) { println!("Rotate");tile.rotate() }
+                if tile.get_borders()[2] != left_neighbor.get_borders()[0] { println!("Flip");tile.flip() }
+                println!("Add tile");
+                solved_tiles_row.push(tile);
+            }
+        }
+        solved_tiles.push(solved_tiles_row);
+    }
+    
+    return solved_tiles;
+}
+
+fn print_solved_puzzle(tiles: &Vec<Vec<puzzle::Tile>>) {
+    
+    for line_with_tiles in tiles {
+        if line_with_tiles.len() > 0 {
+            for idx in 1..line_with_tiles[0].tile_description.len()-1 {
+                let mut puzzle_line : Vec<String>;
+                for tile in line_with_tiles {
+                    print!("{}",tile.tile_description[idx][1..tile.tile_description[idx].len()-1].to_string());
+                }
+                println!();
+            }
         }
     }
-
-
-    return vec![String::from("");10];
 }
 
 fn main() {
     //let input_file = "assets/dec_20.in";
-    let input_file = "assets/dec_20_example.in";
+    let input_file = "assets/dec_20.in";
     let tiles = parse_input(input_file);
     println!("Solution: {}", determine_corners(&tiles) );
 
-    let puzzle = solve_puzzle_positions(&tiles);
-    print_puzzle(&puzzle);
-    print_puzzle_pieces(&puzzle, &tiles);
-    
-    let solved_puzzle = determine_puzzle_output(&puzzle, &tiles);
-    println!("Puzzle IDs:");
+    // Solution 2
+    let puzzle_positions = solve_puzzle_positions(&tiles);
+    print_puzzle(&puzzle_positions);
+    print_puzzle_pieces(&puzzle_positions, &tiles);
 
-    
+    let solved_tiles = solve_puzzle_tiles(&puzzle_positions, tiles);
+    println!("Solved tiles length: {}", solved_tiles.len());
+    print_puzzle_tiles(&solved_tiles);
+    print_solved_puzzle(&solved_tiles);
+
+    //let puzzle_lines : Vec<String> = puzzle.iter().map(|l| l.join("")).collect();
+    //println!("Solved puzzle:\r\n{}", puzzle_lines.join("\r\n"));
 }
 
 #[cfg(test)]
--- 2020/dec_20/src/puzzle.rs
+++ 2020/dec_20/src/puzzle.rs
@@ -1,7 +1,7 @@
+#[derive(Clone)]
 pub struct Tile {
     pub id: u32,
     pub tile_description: Vec<String>,
-    pub borders: Vec<Vec<char>>,
 }
 
 impl Tile {
@@ -12,39 +12,83 @@ impl Tile {
         for desc in &desc[1..] {
             tile_description.push(desc.to_string());
         }
+       
+        Tile { id: id, tile_description: tile_description }
+    }
+
+    // Border order: right, bottom, left, top
+    // Bottom en top border are described left to right
+    // Left and right border are described top to bottom
+    pub fn get_borders(&self) -> Vec<String> {
         let mut borders = Vec::new();
         {
-            let mut left_border = Vec::new();
-            let mut right_border = Vec::new();
-            for i in 1..11 {
-                left_border.push(desc[i].chars().nth(0).unwrap());
-                right_border.push(desc[i].chars().nth(9).unwrap());
+            let mut left_border = "".to_string();
+            let mut right_border = "".to_string();
+            for i in 0..self.tile_description.len() {
+                left_border += &self.tile_description[i].chars().nth(0).unwrap().to_string();
+                right_border += &self.tile_description[i].chars().nth(self.tile_description[i].len()-1).unwrap().to_string();
             }
             borders.push(right_border);
-            borders.push(desc[10].chars().collect()); // Bottom row
+            borders.push(self.tile_description[self.tile_description.len()-1].chars().collect()); // Bottom row
             borders.push(left_border);
         }
-        borders.push(desc[1].chars().collect()); // Top row
+        borders.push(self.tile_description[0].chars().collect()); // Top row
+
+        return borders;
+    }
+
+    pub fn flip_and_rotate_back(&mut self) {
+        self.flip();
+        self.rotate();
+        self.rotate();
+    }
+
+    // Rotates tile by 90 degrees clock wise
+    pub fn rotate(&mut self) {
+        let mut new_tile_description : Vec<String> = vec!["".to_string();self.tile_description.len()];
+        let mut current_tile_description = self.tile_description.to_owned();
+        current_tile_description.reverse();
+        for (_,line) in current_tile_description.iter().enumerate() {
+            for (c_idx,c) in line.chars().enumerate() {
+                new_tile_description[c_idx] += &c.to_string();
+            }
+        }
+        self.tile_description = new_tile_description;
+    }
+    // Rotates tile by 90 degrees counter clockwise
+    pub fn rotate_back(&mut self) {
+        let mut new_tile_description : Vec<String> = vec!["".to_string();self.tile_description.len()];
+        let mut current_tile_description = self.tile_description.to_owned();
         
-        Tile { id: id, tile_description: tile_description, borders: borders}
+        for (_,line) in current_tile_description.iter().enumerate() {
+            for (c_idx,c) in line.chars().enumerate() {
+                new_tile_description[c_idx] += &c.to_string();
+            }
+        }
+        self.tile_description = new_tile_description;
+    }
+
+
+    pub fn flip(&mut self) {
+        let mut new_tile_description = self.tile_description.to_owned();
+        new_tile_description.reverse();
+        self.tile_description = new_tile_description;
     }
 
     pub fn matches_border(tile_a: &Tile, tile_b: &Tile) -> bool {
-        for b in &tile_a.borders {
-            if tile_b.borders.contains(&b) { return true }
-            let mut b_reverse = b.to_owned();
-            b_reverse.reverse();
-            if tile_b.borders.contains(&b_reverse) { return true }
+        let borders = &tile_a.get_borders();
+        for b in borders {
+            if tile_b.get_borders().contains(&b) { return true }
+            if tile_b.get_borders().contains(&b.chars().rev().collect()) { return true }
         }
         return false;
     }
 
-    pub fn common_border(tile_a: &Tile, tile_b: &Tile) -> Vec<char> {
-        for b in &tile_a.borders {
-            if tile_b.borders.contains(&b) { return b.to_vec() }
-            let mut b_reverse = b.to_owned();
-            b_reverse.reverse();
-            if tile_b.borders.contains(&b_reverse) { return b.to_vec() }
+    pub fn common_border(tile_a: &Tile, tile_b: &Tile) -> String {
+        let borders = tile_a.get_borders().to_owned();
+        for b in borders {
+            if tile_b.get_borders().contains(&b) { return b }
+            if tile_b.get_borders().contains(&b.chars().rev().collect()) { return b }
         }
         panic!("No common border found");
     }
@@ -55,8 +99,10 @@ impl Tile {
     pub fn find_common_border(tile_a: &Tile, tile_b: &Tile) -> (usize,usize) {
 
         let common_border = Tile::common_border(tile_a, tile_b);
-        let border_id_tile_a = tile_a.borders.iter().position(|x| *x == common_border).unwrap();
-        let border_id_tile_b = tile_b.borders.iter().position(|x| *x == common_border).unwrap();
+        let mut common_border_reverse = common_border.to_owned();
+        common_border_reverse = common_border_reverse.chars().rev().collect();
+        let border_id_tile_a = tile_a.get_borders().iter().position(|x| *x == common_border || *x == common_border_reverse).unwrap();
+        let border_id_tile_b = tile_b.get_borders().iter().position(|x| *x == common_border || *x == common_border_reverse).unwrap();
         
         return (border_id_tile_a,border_id_tile_b);
     }
@@ -85,9 +131,50 @@ mod tests {
         assert_eq!(true, Tile::matches_border(&tile_a, &tile_b));
         assert_eq!(true, Tile::matches_border(&tile_b, &tile_a));
         
-        assert_eq!(false, Tile::matches_border(&tile_b, &tile_i));
         assert_eq!(false, Tile::matches_border(&tile_a, &tile_i));
+        assert_eq!(false, Tile::matches_border(&tile_b, &tile_i));
+        assert_eq!(false, Tile::matches_border(&tile_e, &tile_i));
         
         assert_eq!(true, Tile::matches_border(&tile_h, &tile_i));
+    }
+
+    #[test]
+    fn validate_get_borders() {
+        let tile = Tile::from_str("Tile 1:\r\n###\r\n...\r\n...");
+        assert_eq!(String::from("#.."), tile.get_borders()[0]);
+        assert_eq!(String::from("..."), tile.get_borders()[1]);
+        assert_eq!(String::from("#.."), tile.get_borders()[2]);
+        assert_eq!(String::from("###"), tile.get_borders()[3]);
+    }
+    #[test]
+    fn validate_rotate() {
+        let mut tile = Tile::from_str("Tile 1:\r\n###\r\n...\r\n...");
+        assert_eq!(String::from("#.."), tile.get_borders()[0]);
+        assert_eq!(String::from("..."), tile.get_borders()[1]);
+        assert_eq!(String::from("#.."), tile.get_borders()[2]);
+        assert_eq!(String::from("###"), tile.get_borders()[3]);
+        
+        tile.rotate();
+        assert_eq!(vec!["..#","..#","..#",], tile.tile_description);
+        assert_eq!(String::from("###"), tile.get_borders()[0]);
+        assert_eq!(String::from("..#"), tile.get_borders()[1]);
+        assert_eq!(String::from("..."), tile.get_borders()[2]);
+        assert_eq!(String::from("..#"), tile.get_borders()[3]);
+    }
+
+    #[test]
+    fn validate_flip() {
+        let mut tile = Tile::from_str("Tile 1:\r\n###\r\n...\r\n...");
+        assert_eq!(String::from("#.."), tile.get_borders()[0]);
+        assert_eq!(String::from("..."), tile.get_borders()[1]);
+        assert_eq!(String::from("#.."), tile.get_borders()[2]);
+        assert_eq!(String::from("###"), tile.get_borders()[3]);
+
+        tile.flip();
+        assert_eq!(vec!["...","...","###",], tile.tile_description);
+        assert_eq!(String::from("..#"), tile.get_borders()[0]);
+        assert_eq!(String::from("###"), tile.get_borders()[1]);
+        assert_eq!(String::from("..#"), tile.get_borders()[2]);
+        assert_eq!(String::from("..."), tile.get_borders()[3]);
     }
 }
