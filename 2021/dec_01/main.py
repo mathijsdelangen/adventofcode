2c2ff412a78da4797b7908cf6939ea786650d0a8
Mathijs de Langen <mathijs.dla@gmail.com>
2021-12-01 18:42:38 +0100

    dec_01: generic layout

--- 2021/dec_01/main.py
+++ 2021/dec_01/main.py
@@ -1,10 +1,10 @@
 import os
 import pathlib
 
-def ReadFile(file):
+def readinput(file):
   return [int(x) for x in open(os.path.join(pathlib.Path(__file__).parent.resolve(),file), 'r').read().splitlines()]
 
-def CalculateSolution(depths):
+def solution(depths):
   count = 0
   for i in range(3, len(depths)):
     if depths[i]+depths[i-1]+depths[i-2] > depths[i-1]+depths[i-2]+depths[i-3]:
@@ -13,5 +13,5 @@ def CalculateSolution(depths):
   return count
 
 if __name__ == '__main__':
-  sol = CalculateSolution(ReadFile("inputs/dec_01.in"))
+  sol = solution(readinput("inputs/input.in"))
   print(f"Total number of depth increases: {sol}")
--- 2021/dec_01/test.py
+++ 2021/dec_01/test.py
@@ -1,14 +1,14 @@
 import unittest
-from dec_01 import ReadFile, CalculateSolution
+from main import readinput, solution
 
 class TestMethods(unittest.TestCase):
 
   def test_example1(self):
-    sol = CalculateSolution(ReadFile("inputs/dec_01_example.in"))
+    sol = solution(readinput("inputs/example.in"))
     #self.assertEqual(sol, 7)
 
   def test_example2(self):
-    sol = CalculateSolution(ReadFile("inputs/dec_01_example.in"))
+    sol = solution(readinput("inputs/example.in"))
     self.assertEqual(sol, 5)
 
 if __name__ == '__main__':
