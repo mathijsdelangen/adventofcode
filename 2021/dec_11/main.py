0cd2365164a4e324495c4a4391090bf1f9fe601f
Mathijs de Langen <mathijs.dla@gmail.com>
2021-12-12 22:07:15 +0100

    2021/dec_11: First solution

---
+++ 2021/dec_11/main.py
@@ -0,0 +1,41 @@
+import os
+import pathlib
+
+def readfile(file):
+  return [[int(x) for x in list(line)] for line in pathlib.Path(os.path.join(pathlib.Path(__file__).parent.resolve(), "inputs", file)).read_text().splitlines()]
+
+def readinput():
+  return readfile("input.in")
+
+def increase_level(x, y, data):
+  data[y][x] += 1
+  if data[y][x] == 10: # First increase > 9, update adjecent tiles
+    for adj_y in range(max(y-1,0), min(y+2, len(data))):
+      for adj_x in range(max(x-1,0), min(x+2, len(data[0]))):
+        data = increase_level(adj_x, adj_y, data)
+  return data
+
+def solution1(data):
+  flashes = 0
+  for _ in range(0, 100):
+    # Increase levels
+    for y in range(0, len(data)):
+      for x in range(0, len(data[0])):
+        data = increase_level(x, y, data)
+  
+    # Flash
+    for y in range(0, len(data)):
+      for x in range(0, len(data[0])):
+        if data[y][x] > 9:
+          flashes += 1
+          data[y][x] = 0
+
+  return flashes
+
+def solution2(data):
+  return data
+
+if __name__ == '__main__':
+  data = readinput()
+  print(f"Solution 1: {solution1(data)}")
+  #print(f"Solution 2: {solution2(data)}")
