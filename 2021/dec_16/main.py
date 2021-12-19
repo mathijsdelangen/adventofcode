1d86916157c5ebce4d7a7e7cd0754b327ed1aabc
Mathijs de Langen <mathijs.dla@gmail.com>
2021-12-19 14:13:37 +0100

    2021/dec_16: Add sum calculation

--- 2021/dec_16/main.py
+++ 2021/dec_16/main.py
@@ -7,6 +7,9 @@ def readfile(file):
 def readinput():
   return readfile("input.in")
 
+def bin_to_int(bin):
+  return int(bin, 2)
+  
 def hex_to_bin(hex):
   int_value = int(hex, 16)
   width = len(hex) * 4
@@ -36,25 +39,71 @@ def calculate_version_sum(packet):
   type_id = int(packet[3:6], 2)
   print(f"Type ID: {type_id}")
 
-  next_idx = 6
+  packet_idx = 6
   # Parse type
   if type_id == 4:
     literal_value, end_idx = get_literal_value(packet[6:])
     print(f"Literal value: {literal_value}")
-    next_idx += end_idx
+    packet_idx += end_idx
   else:
-    next_idx += 16 if packet[6] == '0' else 12
+    packet_idx += 16 if packet[6] == '0' else 12
   
-  return version_sum + calculate_version_sum(packet[next_idx:])
+  return version_sum + calculate_version_sum(packet[packet_idx:])
 
 def evaluate_expression(packet):
-  return 0
+  #if len(packet) < 7:
+  #  return 0
+  
+  # First three bits is the version
+  version_sum = int(packet[0:3], 2)
+  print(f"Version: {int(packet[0:3], 2)}")
+
+  # Next three bits is packet type ID
+  type_id = int(packet[3:6], 2)
+  print(f"Type ID: {type_id}")
+
+  packet_idx = 6
+  # Parse type
+  if type_id == 4:
+    literal_value, end_idx = get_literal_value(packet[6:])
+    print(f"Literal value: {literal_value}")
+    return (literal_value, packet_idx+end_idx)
+  else:
+    packet_idx += 1
+    # Determine length of sub packets
+    length = 15 if packet[6] == '0' else 11
+    nr_packets = bin_to_int(packet[packet_idx:packet_idx+length])
+    packet_idx += length
+    if type_id == 0: # sum
+      print(f"Calculate sum")
+      res = 0
+      for _ in range(nr_packets):
+        val, packet_length = evaluate_expression(packet[packet_idx:])
+        res += val
+        packet_idx += packet_length
+      return (res, packet_idx)
+    elif type_id == 1: # product
+      pass
+    elif type_id == 2: # min
+      pass
+    elif type_id == 3: # max
+      pass
+    elif type_id == 6: # less than
+      pass
+    elif type_id == 7: # equal to
+      pass
+    else: 
+      print(f"Error: Unknown packet type: {type_id}")
+      exit(1)
+    packet_idx += 16 if packet[6] == '0' else 12
+  
+  return (0, packet_idx)
 
 def solution1(data):
   return evaluate_expression(data)
 
 def solution2(data):
-  return data
+  return evaluate_expression(data)[0]
 
 if __name__ == '__main__':
   data = readinput()
